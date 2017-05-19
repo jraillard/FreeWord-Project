047bbfe3d5cf306c3ed08d54a9a461ff8a5444d3
jraillard <ju.raillard@hotmail.fr>
2017-05-19 19:50:40 +0300

    First Test
    Drag And Drop management
    (can still put many PlayedCard on a PlacedCard)

---
+++ FreeWord-Unity3d/Assets/Scripts/DragAndDrop.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragAndDrop : MonoBehaviour {
+
+    private float distance = 5f;
+    private Vector3 originPlace;
+
+    private void Start()
+    {
+        //store the original place
+        originPlace = transform.position;
+        print(originPlace);
+        
+    }
+
+    private void OnMouseDrag()
+    {
+        Vector3 mousePosition= new Vector3 (Input.mousePosition.x, Input.mousePosition.y, distance);
+        Vector3 objPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        transform.position = objPosition;
+        //print("Dragging");  
+    }
+
+    private void OnMouseExit()
+    {
+        if (transform.position.z == 5)
+        {
+            //== 4 if we're on a PlacedCard 
+
+            //print("Replacing");
+            transform.position = originPlace;
+        }
+    }
+}
---
+++ FreeWord-Unity3d/Assets/Scripts/PlacingCard.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlacingCard : MonoBehaviour {
+
+    /* first might not be usefull 
+     * only if the user put the card at the precise 
+     * moment where the PlayedCard touch the PlacedCard
+    */
+    
+    private void OnTriggerEnter(Collider other) 
+    {
+        //Debug.Log("Detect Collision1);
+
+        if (other.tag == "PlayedCard")
+        {
+            //Debug.Log("Placed1");
+            other.transform.position = new Vector3(transform.position.x, transform.position.y, 4);
+        }
+    }
+    
+    private void OnTriggerStay(Collider other)
+    {
+        //Debug.Log("Detect Collision2");
+
+        if (other.tag == "PlayedCard")
+        {
+            //Debug.Log("Placed2");
+            other.transform.position = new Vector3(transform.position.x, transform.position.y, 4);
+        }
+    }
+    
+}
