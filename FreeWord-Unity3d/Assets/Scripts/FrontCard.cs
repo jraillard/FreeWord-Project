829211dc2c1dd78cc22de00c356e19e50971fa06
jraillard <ju.raillard@hotmail.fr>
2017-05-29 20:36:46 +0300

    UnityApp_Patch6
    Played selection possibility set
    New card shapes tested
    Pb to solve :
    =>PlayedCard default placement after Drag&Drop

---
+++ FreeWord-Unity3d/Assets/Scripts/FrontCard.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrontCard : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag=="PlayedCard")
+        {
+            print("coucou");
+            gameObject.GetComponentInParent<PlayedCard>().SetSelectable(false);
+            print(gameObject.GetComponentInParent<PlayedCard>().IsSelectable());
+            //gameObject.GetComponent<BoxCollider>().size = new Vector3(0, 0, 0);
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "PlayedCard")
+        {
+            print("coucou");
+            gameObject.GetComponentInParent<PlayedCard>().SetSelectable(false);
+            print(gameObject.GetComponentInParent<PlayedCard>().IsSelectable());
+            //gameObject.GetComponent<BoxCollider>().size = new Vector3(0, 0, 0);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "PlayedCard")
+        {
+            print("byebye");
+            gameObject.GetComponentInParent<PlayedCard>().SetSelectable(true);
+            print(gameObject.GetComponentInParent<PlayedCard>().IsSelectable());
+            //gameObject.GetComponent<BoxCollider>().size = new Vector3(1, 1, 1);
+        }
+    }
+}
