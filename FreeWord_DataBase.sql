2527f4ba7694c13e69a8027738227089278fc92b
jraillard <ju.raillard@hotmail.fr>
2017-06-16 14:48:10 +0300

    DB correct
    Don't need Level table anymore
    added wd_value , wd_nbtime to WordTable

--- FreeWord_DataBase.sql
+++ FreeWord_DataBase.sql
@@ -35,7 +35,7 @@ CREATE TABLE Category
 ( 
 	cat_id int AUTO_INCREMENT, 
 	cat_name varchar(32) not null, 
-	cat_reached boolean not null, 
+	cat_reached boolean not null, /* all word discover or not */
 	cat_lng int not null,
 	
 	CONSTRAINT pk_catId primary key(cat_id),
@@ -46,14 +46,17 @@ CREATE TABLE Category
 CREATE TABLE Word 
 ( 
 	wd_id int AUTO_INCREMENT, 
-	wd_level integer(3) not null, 
-	wd_cat int not null,  
+	wd_value varchar(30) not null, /* the word */
+	wd_nbtime int not null, /* store the number that the user */
+	wd_cat int not null,
+    	
 	
 	CONSTRAINT pk_wdId primary key(wd_id),
 	CONSTRAINT fk_wdId foreign key(wd_cat) references Category(cat_id)
-
+	
 );
 
+/*Don't need anymore Level = NbwordsFound-5
 CREATE TABLE Level 
 (
 	lvl_id int AUTO_INCREMENT,
@@ -65,3 +68,4 @@ CREATE TABLE Level
 	CONSTRAINT fk_lvlId foreign key(lvl_cat) references Category(cat_id)
 	
 );
+*/
